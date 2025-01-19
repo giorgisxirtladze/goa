83d7eeb9f03016c2f035405a752cfd56348992e4
giorgisxirtladze <lela.songulashvili@gmail.com>
2025-01-19 14:19:22 +0400

    level 34

---
+++ level 34/classwork/lesson.py
@@ -0,0 +1,17 @@
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+result1 = multiply_numbers(2, 3)
+print("Result 1:", result1)
+
+result2 = multiply_numbers(4, 5)
+print("Result 2:", result2)
+
+result3 = multiply_numbers(6, 7)
+print("Result 3:", result3)
+
+result4 = multiply_numbers(8, 9)
+print("Result 4:", result4)
+
+result5 = multiply_numbers(10, 11)
+print("Result 5:", result5)
---
+++ level 34/classwork/second.py
@@ -0,0 +1,9 @@
+def sum_or_multiply(num1, num2):
+    if num1 > 50:
+        print(num1 * num2)
+    else:
+        print(num1 + num2)
+
+sum_or_multiply(60, 5)
+sum_or_multiply(30, 10)
+sum_or_multiply(50, 20)
