5ae6fd0fee92957583ebfa4ff233eed36e468038
giorgisxirtladze <lela.songulashvili@gmail.com>
2025-02-09 11:36:38 +0400

    level 37

--- level 36/classwork/fouth.py
+++ level 36/classwork/fouth.py
@@ -1,5 +1,5 @@
 
 def big_sentence(name, surname, age, academy, role):
-    print(f"Hello, my name is {name}, my surname is {surname}. I am {age} years old. I study at {academy}, my role is {role}.")
+    print("Hello, my name is {name}, my surname is {surname}. I am {age} years old. I study at {academy}, my role is {role}.")
 
 big_sentence("გიორგი", "სხირტლაძე", 11, "goal oriented academy", "მოსწავლე")
